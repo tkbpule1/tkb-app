34c5c704593d066264e09879457bcbeda35e9660
Tim <tkbpule1@gmail.com>
2016-07-05 22:08:21 -0400

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  test "should get root" do
+    get :home
+    assert_response :success
+  end
+
   test "should get home" do
     get :home
     assert_response :success
