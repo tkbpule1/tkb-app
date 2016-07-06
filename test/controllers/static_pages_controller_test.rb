e165f86268737e1b86551fdf5fc7f93a03992164
Tim <tkbpule1@gmail.com>
2016-07-06 14:30:10 -0400

    Updated titles and test files for titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,25 +10,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select 'title', "Welcome"
+    assert_select 'title', "Welcome | Mobtown Offroad"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select 'title', "Help"
+    assert_select 'title', "Help | Mobtown Offroad"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select 'title', "About Us"
+    assert_select 'title', "About Us | Mobtown Offroad"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select 'title', "Contact Us"
+    assert_select 'title', "Contact Us | Mobtown Offroad"
   end
 
 
