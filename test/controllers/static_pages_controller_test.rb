0196d0245553365d2afc7969845a5a9eb6f712be
Tim <tkbpule1@gmail.com>
2016-07-05 22:04:36 -0400

    Added contact page && updated application controller

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select 'title', "About Us"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select 'title', "Contact Us"
+  end
+
+
 end
