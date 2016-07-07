b0d10054cf86524af70720e7cce2748f43ee53da
Tim <tkbpule1@gmail.com>
2016-07-07 16:41:48 -0400

    Add function to signup button
    Add create definition to user controller.  Add scss style to eror
    messages. Render error messages.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     # debugger
   end
+
+  def create
+    @user = User.new(params[user_params])
+    if @user.save
+      @user = User.find(params[:id])
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
