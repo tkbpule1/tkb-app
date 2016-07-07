deb9a4d6df42f4f7c3b974c5f995976626cc0680
Tim <tkbpule1@gmail.com>
2016-07-07 11:18:42 -0400

    Add Tests to Users Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 100 },
+                    format: { with: VALID_EMAIL },
+                    uniqueness: { case_sensitive: false }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,5 +9,49 @@ class UserTest < ActiveSupport::TestCase
   test 'should be valid' do
     assert @user.valid?
   end
-  
+
+  test 'name should be present' do
+    @user.name = '   '
+    assert_not @user.valid?
+  end
+
+  test 'name should not be too long' do
+    @user.name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  test 'email should be present' do
+    @user.email = '   '
+    assert_not @user.valid?
+  end
+
+  test 'email should not be too long' do
+    @user.email = 'a' * 101
+    assert_not @user.valid?
+  end
+
+  test 'email should be valid' do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US_ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.com]
+    valid_addresses.each do |valid|
+      @user.email = valid
+      assert @user.valid?, "#{valid.inspect} should be valid"
+    end
+  end
+
+  test 'email should be rejected if invalid' do
+    invalid_addresses = %w[user@example,com USER_at_foo.COM
+                           A_US_ER@foo. first@foo_baz.jp alice@foo+baz.com]
+    invalid_addresses.each do |invalid|
+      @user.email = invalid
+      assert @user.invalid?, "#{invalid.inspect} should be invalid"
+    end
+  end
+
+  test 'email should be unique' do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
 end
