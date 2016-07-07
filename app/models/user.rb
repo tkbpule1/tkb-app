1699408838e1f0a1740846c2fd4070d7e2d65a4b
Tim <tkbpule1@gmail.com>
2016-07-07 12:54:46 -0400

    Secure Password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL },
                     uniqueness: { case_sensitive: false }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -62,4 +62,14 @@ class UserTest < ActiveSupport::TestCase
     assert_equal email.downcase, @user.reload.email
   end
 
+  test 'password should be present' do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test 'password should have a min length' do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
 end
